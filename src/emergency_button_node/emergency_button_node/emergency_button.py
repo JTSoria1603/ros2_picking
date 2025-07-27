d95449190f16655dff30b48f2e8364cd5d4c660c
Tomas Soria <juantomas.soria@outlook.es>
2025-07-27 20:43:33 -0300

    emergency button node

---
+++ src/emergency_button_node/emergency_button_node/emergency_button.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Bool
+from std_srvs.srv import Trigger
+
+class EmergencyButtonNode(Node):
+    def __init__(self):
+        super().__init__('emergency_button_node')
+        self.pub = self.create_publisher(Bool, 'emergency_button', 10)
+        self.state = False 
+        self.timer = self.create_timer(0.5, self.publish_state)
+        self.get_logger().info('Emergency Button Node has been started')
+        self.press_srv   = self.create_service(Trigger, 'press_emergency_button',   self.handle_press)
+        self.release_srv = self.create_service(Trigger, 'release_emergency_button', self.handle_release)
+        self.get_emergency_button_status = self.create_service(Trigger, 'get_emergency_button_state', self.handle_get_emergency_button_status)
+
+    def publish_state(self):
+        msg = Bool(data=self.state)
+        self.pub.publish(msg)
+        self.get_logger().info(f'Published emergency_button: {self.state}')
+
+    def handle_get_emergency_button_status(self, request, response):
+        response.success = True
+        response.message = str(self.state)
+        self.get_logger().info(f'Current emergency button state: {self.state}')
+        return response
+
+    def handle_press(self, request, response):
+        if not self.state:
+            self.state = True
+            response.success = True
+            response.message = 'Emergency button PRESSED'
+            self.get_logger().info('Emergency button pressed → robot must stop')
+        else:
+            response.success = False
+            response.message = 'Already PRESSED'
+        return response
+
+    def handle_release(self, request, response):
+        if self.state:
+            self.state = False
+            response.success = True
+            response.message = 'Emergency button RELEASED'
+            self.get_logger().info('Emergency button released → robot may resume')
+        else:
+            response.success = False
+            response.message = 'Already RELEASED'
+        return response
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = EmergencyButtonNode()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
